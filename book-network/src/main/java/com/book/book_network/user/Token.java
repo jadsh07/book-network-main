bf2cf97afa091b35f253285e6610e2953383f109
Jadshan <j.manokaran@homeit-system.de>
2024-11-24 16:02:29 +0530

    Add Token entity and TokenRepository for token management functionality

---
+++ book-network/src/main/java/com/book/book_network/user/Token.java
@@ -0,0 +1,32 @@
+package com.book.book_network.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
