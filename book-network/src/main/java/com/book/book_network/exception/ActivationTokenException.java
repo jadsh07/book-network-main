f13cc99cc2954f6074b6c2c8cc264ea1469baaaa
Jadshan <j.manokaran@homeit-system.de>
2024-11-26 08:20:15 +0530

    Add custom exceptions and global exception handler for improved error management

---
+++ book-network/src/main/java/com/book/book_network/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.book.book_network.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
