f1ca502804ea44409614419d80cdc47526ce0308
Jadshan <j.manokaran@homeit-system.de>
2024-12-06 14:08:20 +0530

    Refactor account activation logic and improve token validation in AuthenticationService

--- book-network/src/main/java/com/book/book_network/auth/AuthenticationService.java
+++ book-network/src/main/java/com/book/book_network/auth/AuthenticationService.java
@@ -56,9 +56,26 @@ public class AuthenticationService {
     sendValidationEmail(user);
   }
 
+  public void activateAccount(String token) throws MessagingException {
+    Token savedToken = tokenRepository.findByToken(token)
+      // todo exception has to be defined
+      .orElseThrow(() -> new RuntimeException("Invalid token"));
+    if (LocalDateTime.now().isAfter(savedToken.getExpiresAt())) {
+      sendValidationEmail(savedToken.getUser());
+      throw new RuntimeException("Activation token has expired. A new token has been send to the same email address");
+    }
+
+    var user = userRepository.findById(savedToken.getUser().getId())
+      .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    user.setEnabled(true);
+    userRepository.save(user);
+
+    savedToken.setValidatedAt(LocalDateTime.now());
+    tokenRepository.save(savedToken);
+  }
+
   private void sendValidationEmail(User user) throws MessagingException {
     var newToken = generateAndSaveActivationToken(user);
-
 
     emailService.sendEmail(
       user.getEmail(),
@@ -116,22 +133,6 @@ public class AuthenticationService {
   }
 
  // @Transactional
-  public void activateAccount(String token) throws MessagingException {
-    Token savedToken = tokenRepository.findByToken(token)
-      // todo exception has to be defined
-      .orElseThrow(() -> new RuntimeException("Invalid token"));
-    if (LocalDateTime.now().isAfter(savedToken.getExpiresAt())) {
-      sendValidationEmail(savedToken.getUser());
-      throw new RuntimeException("Activation token has expired. A new token has been send to the same email address");
-    }
 
-    var user = userRepository.findById(savedToken.getUser().getId())
-      .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    user.setEnabled(true);
-    userRepository.save(user);
-
-    savedToken.setValidatedAt(LocalDateTime.now());
-    tokenRepository.save(savedToken);
-  }
 
 }
